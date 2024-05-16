900c2d7eb78e68f64f64fad58f551ab6aa045245
Tovy <Jasper.brilleman@gmail.com>
2024-05-16 12:55:42 +0200

    Connection and webhook test

--- src/main/java/projects/tovy/github/ServerUsage/Shops/ShopFunctions.java
+++ src/main/java/projects/tovy/github/ServerUsage/Shops/ShopFunctions.java
@@ -6,37 +6,22 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import projects.tovy.github.EasyGuiBorder;
+import projects.tovy.github.ItemHandeling;
 
 public class ShopFunctions {
+    private ItemHandeling item;
+    private EasyGuiBorder border;
+
+    public ShopFunctions(ItemHandeling item, EasyGuiBorder border) {
+        this.item = item;
+        this.border = border;
+    }
 
     public void funcShop1() {
 
     }
 
-    public void openGui(Player p) {
-        Inventory gui = Bukkit.createInventory(null, 54, "Custom GUI");
 
-
-        ItemStack grayGlass = new ItemStack(Material.GRAY_STAINED_GLASS_PANE);
-        ItemMeta meta = grayGlass.getItemMeta();
-        if (meta != null) {
-            meta.setDisplayName("");
-            grayGlass.setItemMeta(meta);
-        }
-
-       
-        for (int i = 0; i < 54; i++) {
-            if (isOuterSlot(i)) {
-                gui.setItem(i, grayGlass);
-            }
-        }
-
-        p.openInventory(gui);
-    }
-    private boolean isOuterSlot(int slot) {
-        int row = slot / 9;
-        int col = slot % 9;
-        return row == 0 || row == 5 || col == 0 || col == 8;
-    }
 
 }
