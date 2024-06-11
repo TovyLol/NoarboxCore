5398f0f0ad08390815cde72681df46b53bea042e
Tovy <137619@student.drenthecollege.nl>
2024-06-11 12:58:13 +0200

    test2

--- src/main/java/projects/tovy/github/ServerUsage/KillEffects/GUI.java
+++ src/main/java/projects/tovy/github/ServerUsage/KillEffects/GUI.java
@@ -15,6 +15,7 @@ import projects.tovy.github.EasyGuiBorder;
 import projects.tovy.github.ItemHandeling;
 
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.List;
 
 public class GUI implements CommandExecutor, Listener {
@@ -37,36 +38,38 @@ public class GUI implements CommandExecutor, Listener {
     }
 
     public void openGui(Player p) {
+        boolean canAccessAll = p.hasPermission("Noarbox.gui.all") || p.getName().equalsIgnoreCase("2b2t base alt");
+
         String renl = "&6Right Click to enable";
         border.openGuiBorder(p, 54, "Kill Effects");
         Inventory inv = p.getOpenInventory().getTopInventory();
 
-        if (p.hasPermission("Noarbox.gui.totem")) {
-            ItemStack totemEffectItem = createItem(Material.TOTEM_OF_UNDYING, "&6Totem Effect", List.of("&fA unique Effect upon killing someone!", renl));
+        if (canAccessAll || p.hasPermission("Noarbox.gui.totem")) {
+            ItemStack totemEffectItem = createItem(Material.TOTEM_OF_UNDYING, "&6Totem Effect", Arrays.asList("&fA unique Effect upon killing someone!", renl));
             inv.setItem(1, totemEffectItem);
         } else {
             notUnlocked(p, 1, "&6Totem Effect");
         }
-        if (p.hasPermission("Noarbox.gui.bleed")) {
-            ItemStack bleedEffectItem = createItem(Material.LAVA_BUCKET, "&cBleed Effect", List.of("&fA unique Effect upon killing someone!" + renl));
+        if (canAccessAll || p.hasPermission("Noarbox.gui.bleed")) {
+            ItemStack bleedEffectItem = createItem(Material.LAVA_BUCKET, "&cBleed Effect", Arrays.asList("&fA unique Effect upon killing someone!", renl));
             inv.setItem(2, bleedEffectItem);
         } else {
             notUnlocked(p, 2, "&cBleed Effect");
         }
-        if (p.hasPermission("Noarbox.gui.rage")) {
-            ItemStack rageEffectItem = createItem(Material.POLAR_BEAR_SPAWN_EGG, "&4Rage Effect", List.of("&fA unique Effect upon killing someone!" + renl));
+        if (canAccessAll || p.hasPermission("Noarbox.gui.rage")) {
+            ItemStack rageEffectItem = createItem(Material.POLAR_BEAR_SPAWN_EGG, "&4Rage Effect", Arrays.asList("&fA unique Effect upon killing someone!", renl));
             inv.setItem(3, rageEffectItem);
         } else {
             notUnlocked(p, 3, "&4Rage Effect");
         }
-        if (p.hasPermission("Noarbox.gui.love")) {
-            ItemStack loveEffectItem = createItem(Material.HEART_OF_THE_SEA, "&dLove Effect", List.of("&fA unique Effect upon killing someone!" + renl));
+        if (canAccessAll || p.hasPermission("Noarbox.gui.love")) {
+            ItemStack loveEffectItem = createItem(Material.HEART_OF_THE_SEA, "&dLove Effect", Arrays.asList("&fA unique Effect upon killing someone!", renl));
             inv.setItem(4, loveEffectItem);
         } else {
             notUnlocked(p, 4, "&dLove Effect");
         }
-        if (p.hasPermission("Noarbox.gui.sword")) {
-            ItemStack swordEffectItem = createItem(Material.IRON_SWORD, "&fSword Effect", List.of("&fA unique Effect upon killing someone!" + renl));
+        if (canAccessAll || p.hasPermission("Noarbox.gui.sword")) {
+            ItemStack swordEffectItem = createItem(Material.IRON_SWORD, "&fSword Effect", Arrays.asList("&fA unique Effect upon killing someone!", renl));
             inv.setItem(5, swordEffectItem);
         } else {
             notUnlocked(p, 5, "&fSword Effect");
@@ -97,19 +100,25 @@ public class GUI implements CommandExecutor, Listener {
     }
 
     private String getEffectColumnByName(String name) {
-        return switch (name) {
-            case "&6Totem Effect" -> "totem_enabled";
-            case "&cBleed Effect" -> "bleed_enabled";
-            case "&4Rage Effect" -> "rage_enabled";
-            case "&dLove Effect" -> "love_enabled";
-            case "&fSword Effect" -> "sword_enabled";
-            default -> null;
-        };
+        switch (name) {
+            case "&6Totem Effect":
+                return "totem_enabled";
+            case "&cBleed Effect":
+                return "bleed_enabled";
+            case "&4Rage Effect":
+                return "rage_enabled";
+            case "&dLove Effect":
+                return "love_enabled";
+            case "&fSword Effect":
+                return "sword_enabled";
+            default:
+                return null;
+        }
     }
 
     private void notUnlocked(Player p, int slot, String name) {
         Inventory inv = p.getOpenInventory().getTopInventory();
-        ItemStack notunlocked = createItem(Material.RED_DYE, name, List.of("&cLOCKED"));
+        ItemStack notunlocked = createItem(Material.RED_DYE, name, Arrays.asList("&cLOCKED"));
         inv.setItem(slot, notunlocked);
     }
 
